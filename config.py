befac9da1032e54c5d13906ed2cea7e14cf3fdd6
3ssalunke <67055435+3ssalunke@users.noreply.github.com>
2020-11-02 18:40:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'abcdefghijklmnopqrstuvwxyz'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     MONGODB_SETTINGS = { 'db': 'UTA_Enrollment'}
     
